77b470b5ac16a790d5ea9562c3804e8f5ce27a4d
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-11-02 11:25:10 -0400

    feat: refactor and test scraper

--- scraper.py
+++ scraper.py
@@ -4,39 +4,54 @@
 Uses Anaconda environment "webby" with selenium to load JavaScript on the page
 and get content:
     - Python 3.7, selenium, requests, scrapy, and Beautiful Soup 4 (bs4)
-    - Run `conda activate webby`, `python scraper.py`
+    - Run `conda activate webby` then `python scraper.py`
 '''
 
 
+import csv
 import time
 from selenium import webdriver
 import bs4
 
 
 def main():
-    # TODO: Import and loop over document with all comment link data
+    csv_file_path = './Data/scraper_csv.csv'
+    out_path = './Data/Comments/'
 
-    # TODO: Create loop with scraper code to get all comments
-    url = 'https://www.regulations.gov/document?D=EPA-HQ-OW-2018-0149-3974'
-    # css_path = ('html body div div.printPageContainer.GIY1LSJCQC div'
-    #             '.GIY1LSJNPC.GIY1LSJBQC.printPage div.GIY1LSJAQC div.'
-    #             'GIY1LSJD5C table tbody tr td div div.breakWord div.'
-    #             'GIY1LSJLXD div.GIY1LSJIXD div')
+    scrape_comments(csv_file_path, out_path)
+
+
+def scrape_comments(csv_file_path, out_path):
     css_sel = '.GIY1LSJIXD > div:nth-child(2)'
+    # id_prefix = 'EPA-HQ-OW-2018-0149-'
 
-    browser = webdriver.Firefox()
-    browser.get(url)
-    time.sleep(5)
-    innerHTML = browser.execute_script("return document.body.innerHTML")
-    soup = bs4.BeautifulSoup(innerHTML, features="lxml")
-    elem = soup.select(css_sel)
-    print(len(elem))
+    with open(csv_file_path, newline='') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)  # Skip header
+        for i, row in enumerate(csvreader):
+            if i > 10:
+                break
 
-    # TODO: Store comments in database or file
-    comment = elem[0].text
-    print(comment)
-    time.sleep(2)
-    browser.quit()
+            ID = row[0]
+            url = row[1]
+
+            browser = webdriver.Firefox()
+            browser.get(url)
+            time.sleep(4)  # Allow time for page to load
+            innerHTML = browser.execute_script("return document.body.innerHTML")
+            soup = bs4.BeautifulSoup(innerHTML, features="lxml")
+            elem = soup.select(css_sel)
+            comment = elem[0].text
+            # print(comment)
+
+            # Save ID and comment to a text file
+            file_path = out_path + '{}.txt'.format(ID)
+            with open(file_path, 'w') as f:
+                f.write(ID + '\n')
+                f.write(comment)
+
+            time.sleep(1)
+            browser.quit()
 
 
 if __name__ == '__main__':
