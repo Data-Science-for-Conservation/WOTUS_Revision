d02083c6ac36f0d6499b3f766b6ec11d73632111
Heather Kusmierz <heather.kusmierz@gmail.com>
2020-04-16 17:12:47 -0400

    feat: finish layout for sentiment classifier app

--- deployed_model.py
+++ deployed_model.py
@@ -1,39 +1,61 @@
 #!/usr/bin/env python3
 
 import pickle
+import pandas as pd
 import streamlit as st
 
 
-"""
-TODO:
-- Import model
-- Lay out app page
-- Get user input
-- Show class and probability output
-"""
-
-
-# Import model
 @st.cache
-def get_model():
+def get_clf_model():
     with open('final_sentiment_clf.pkl', 'rb') as f:
         model = pickle.load(f)
 
     return model
 
 
+st.title('Sentiment Classifier Demo')
 st.markdown(
 """
-# Model Demo
-
 Welcome to the live version of the WOTUS sentiment classifier!
 
-For a full overview and summary of the project, visit the [GitHub page here]()
-or find the
+Enter your comment about the EPA and Army's proposed re-definition of "Waters
+of the United States" and see whether the model classifies it as "Opposing" or
+"Supportive" of the rule change, along with the probability of receiving that
+label.
+
+Additional project resources include a full
+[project writeup](https://data-science-for-conservation.github.io/WOTUS_Revision/)
+or the
 [source code](https://github.com/Data-Science-for-Conservation/WOTUS_Revision)
 that generated the model.
 """)
 
-sent_clf = get_model()
+sent_clf = get_clf_model()
 
-# TODO: Get user input
+classes = {0: "Opposing",
+           1: "Supportive"}
+
+
+def run_sent_analysis(comment):
+    pred = sent_clf.predict_proba(comment)
+    label = classes[pred[0].argmax()]
+    prob = round(pred[0].max(), 3)
+
+    results = pd.DataFrame([label, prob],
+                           index=['Sentiment', 'Confidence'],
+                           columns=['Value'])
+
+    return results
+
+
+comment = st.text_area('Enter your comment here',
+    """
+    It is important for water quality to protect our wetlands and waterways.
+    We should be increasing protection not decreasing it.
+    """)
+
+try:
+    result = run_sent_analysis(comment)
+    st.table(result)
+except ValueError as e:
+    st.write(e)
