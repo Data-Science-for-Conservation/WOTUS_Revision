bb7e8a359523f91bf68720c6fba1e569d204faf6
Heather Kusmierz <heather.kusmierz@gmail.com>
2020-04-17 21:39:39 -0400

    feat: update tokenizer to use app-friendly imports

--- Tokenizer/tokenizer.py
+++ Tokenizer/tokenizer.py
@@ -1,4 +1,5 @@
 import spacy
+import en_core_web_sm
 
 
 def external_spacy_tokenizer(doc):
@@ -13,7 +14,6 @@ def external_spacy_tokenizer(doc):
     # tokens = lemmatizer(doc)
     # return([token.lemma_ for token in tokens
     #         if token.lemma_.isalnum()])
-    nlp = spacy.load('en_core_web_sm',
-                     disable=['tagger', 'parser', 'ner'])
+    nlp = en_core_web_sm.load(disable=['tagger', 'parser', 'ner'])
     return ([token.lemma_ for token in nlp(doc)
              if (token.lemma_.isalnum() and not token.is_stop)])
